52c98a80b5e1e9e443e83972ebcf611aa44d0eee
reuvkess <77792132+reuvkess@users.noreply.github.com>
2021-01-21 19:19:42 +0200

    Add files via upload

---
+++ rpc2js.js
@@ -0,0 +1,36 @@
+function getPass() {
+    var put = prompt('enter password: ');
+    if (put === 'rrr009') {
+        document.getElementById('but').style.display = 'block';
+    }
+    if (put !== 'rrr009') {
+        alert('Password incorrect');
+        getPass();
+    }
+}
+ctb = function () {
+    document.getElementById('bod').style.backgroundColor="lightskyblue";
+}
+ctr = function () {
+    document.getElementById('bod').style.backgroundColor = "indianred";
+}
+ctg = function () {
+    document.getElementById('bod').style.backgroundColor="darkseagreen";
+}
+var t;
+t = 0;
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'b') {
+        document.getElementById('bod').style.backgroundColor="lightskyblue";
+    }
+});
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'g') {
+        document.getElementById('bod').style.backgroundColor="darkseagreen";
+    }
+});
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'r') {
+        document.getElementById('bod').style.backgroundColor="indianred";
+    }
+});
