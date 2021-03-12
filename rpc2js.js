717423cf86fcab78718c8965403e5488fefd9696
reuvkess <77792132+reuvkess@users.noreply.github.com>
2021-03-12 18:10:36 +0200

    Update rpc2js.js

--- rpc2js.js
+++ rpc2js.js
@@ -1,3 +1,1 @@
-ctb=function(){document.body.style.background="#1E90FF";document.getElementsByTagName('h1')[0].background="#CD5C5C";}
-ctr=function(){document.body.style.background="#CD5C5C";document.getElementsByTagName('h1')[0].background="#1E90FF";}
-ctg=function(){document.body.style.background="#71BC78";document.getElementsByTagName('h1')[0].background="#1E90FF";}
+
