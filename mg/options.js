2b92da6d2c6bf09af61a836f948c05f7b6925083
reuven2009 <87575824+reuven2009@users.noreply.github.com>
2021-10-26 19:07:52 +0300

    Add files via upload

---
+++ mg/options.js
@@ -0,0 +1,11 @@
+function addon() {
+    window.open("mg.html");
+    window.open("get_uuid.html");
+}
+function single_pack() {
+    window.open("mg.html")
+}
+function skin_pack() {
+    window.open("sg.html");
+    window.open("mg.html");
+}
